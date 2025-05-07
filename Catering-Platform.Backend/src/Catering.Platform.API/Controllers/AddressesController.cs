30904afa9407421b47c14ff33a3e9538f0c24fd7
zola191 <zola19@yandex.ru>
2025-05-07 08:51:36 +0300

    Внесены изменения в валидатор Добавлена миграция по изменению названий таблицы для приведения к единообразию Добавлен валидатор в метод Update

--- Catering-Platform.Backend/src/Catering.Platform.API/Controllers/AddressesController.cs
+++ Catering-Platform.Backend/src/Catering.Platform.API/Controllers/AddressesController.cs
@@ -4,6 +4,7 @@ using Catering.Platform.Applications.ViewModels;
 using Catering.Platform.Domain.Models;
 using Catering.Platform.Domain.Requests.Adress;
 using FluentValidation;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Catering.Platform.API.Controllers;
@@ -13,16 +14,19 @@ namespace Catering.Platform.API.Controllers;
 public class AddressesController : ControllerBase
 {
     private readonly IAddressService _addressService;
-    private readonly IValidator<CreateAddressViewModel> createAdressViewModelValidator;
+    private readonly IValidator<CreateAddressViewModel> _createAdressViewModelValidator;
+    private readonly IValidator<UpdateAddressViewModel> _updateAdressViewModelValidator;
     private readonly ILogger<AddressesController> _logger;
 
     public AddressesController(
         IAddressService addressService,
         IValidator<CreateAddressViewModel> createAdressViewModelValidator,
+        IValidator<UpdateAddressViewModel> updateAdressViewModelValidator,
         ILogger<AddressesController> logger)
     {
         _addressService = addressService;
-        this.createAdressViewModelValidator = createAdressViewModelValidator;
+        _createAdressViewModelValidator = createAdressViewModelValidator;
+        _updateAdressViewModelValidator = updateAdressViewModelValidator;
         _logger = logger;
     }
 
@@ -31,7 +35,7 @@ public class AddressesController : ControllerBase
     [FromRoute] Guid tenantId,
     [FromBody] CreateAddressViewModel request)
     {
-        var validationResult = await createAdressViewModelValidator.ValidateAsync(request);
+        var validationResult = await _createAdressViewModelValidator.ValidateAsync(request);
         if (validationResult.IsValid == false)
         {
             _logger.LogInformation(
@@ -44,12 +48,21 @@ public class AddressesController : ControllerBase
             new Uri($"/api/addresses/{addressViewModel.Id}", UriKind.Relative),
             addressViewModel);
     }
-    // GET /api/addresses/{addressId}
-    [HttpGet("{addressId:guid}")]
-    public async Task<ActionResult> GetById([FromRoute] Guid addressId, [FromBody] UpdateAddressViewModel viewModel)
+    [HttpPut("{addressId:guid}")]
+    public async Task<ActionResult> Update(
+    [FromRoute] Guid addressId, 
+    [FromBody] UpdateAddressViewModel request)
     {
+        var validationResult = await _updateAdressViewModelValidator.ValidateAsync(request);
+        if (validationResult.IsValid == false)
+        {
+            _logger.LogInformation(
+            "Validation failed for UpdateAddressViewModel. Errors: {ValidationErrors}",
+            validationResult.Errors);
+            return BadRequest(validationResult.Errors);
+        }
         var tenantId = Guid.Parse("0196763c-9106-7806-a03f-960a1dad80e7");
-        await _addressService.UpdateAddressAsync(addressId, viewModel, tenantId);
-        return Ok(viewModel);
+        await _addressService.UpdateAddressAsync(addressId, request, tenantId);
+        return Ok(request);
     }
 }
---
+++ Catering-Platform.Backend/src/Catering.Platform.Persistence/Migrations/20250507051911_UpdateModels.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Catering.Platform.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateModels : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_dish_category_CategoryId",
+                table: "dish");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_dish",
+                table: "dish");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_category",
+                table: "category");
+
+            migrationBuilder.RenameTable(
+                name: "dish",
+                newName: "Dish");
+
+            migrationBuilder.RenameTable(
+                name: "category",
+                newName: "Category");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_dish_CategoryId",
+                table: "Dish",
+                newName: "IX_Dish_CategoryId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Dish",
+                table: "Dish",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Category",
+                table: "Category",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Dish_Category_CategoryId",
+                table: "Dish",
+                column: "CategoryId",
+                principalTable: "Category",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Dish_Category_CategoryId",
+                table: "Dish");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Dish",
+                table: "Dish");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Category",
+                table: "Category");
+
+            migrationBuilder.RenameTable(
+                name: "Dish",
+                newName: "dish");
+
+            migrationBuilder.RenameTable(
+                name: "Category",
+                newName: "category");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Dish_CategoryId",
+                table: "dish",
+                newName: "IX_dish_CategoryId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_dish",
+                table: "dish",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_category",
+                table: "category",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_dish_category_CategoryId",
+                table: "dish",
+                column: "CategoryId",
+                principalTable: "category",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
