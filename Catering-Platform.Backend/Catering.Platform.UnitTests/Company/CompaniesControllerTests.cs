ddf949328af5271f12bd77d06db740f02c3608b2
zola191 <zola19@yandex.ru>
2025-07-08 14:30:05 +0300

    - добавлен контроллер - добавлен валидатор - добавлены тесты на контроллер и сервис

---
+++ Catering-Platform.Backend/Catering.Platform.UnitTests/Company/CompaniesControllerTests.cs
@@ -0,0 +1,63 @@
+﻿using AutoFixture;
+using Catering.Platform.API.Controllers;
+using Catering.Platform.Applications.Abstractions;
+using Catering.Platform.Applications.ViewModels.Company;
+using Catering.Platform.Domain.Requests.Company;
+using FluentAssertions;
+using FluentValidation;
+using FluentValidation.Results;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using NSubstitute;
+
+namespace Catering.Platform.UnitTests.Company;
+
+public class CompaniesControllerTests
+{
+    private readonly IValidator<CreateCompanyRequest> _mockCreateCompanyvalidator;
+    private readonly ICompanyService _mockCompanyService;
+    private readonly ILogger<CompaniesController> _mockLogger;
+    private readonly CompaniesController _controller;
+    private readonly Fixture _fixture;
+
+    public CompaniesControllerTests()
+    {
+        _mockCreateCompanyvalidator = Substitute.For<IValidator<CreateCompanyRequest>>();
+        _mockCompanyService = Substitute.For<ICompanyService>();
+        _mockLogger = NullLogger<CompaniesController>.Instance;
+
+        _controller = new CompaniesController(
+            _mockCreateCompanyvalidator,
+            _mockCompanyService,
+            _mockLogger);
+
+        _fixture = new Fixture();
+    }
+
+    [Fact]
+    public async Task Create_ReturnsCorrectResponse_WhenServiceReturnsViewModel()
+    {
+        // Arrange
+        var request = _fixture.Create<CreateCompanyRequest>();
+        var companyViewModel = _fixture.Create<CompanyViewModel>();
+        var validationResult = new ValidationResult();
+
+        _mockCreateCompanyvalidator.ValidateAsync(request)
+            .Returns(Task.FromResult(validationResult));
+
+        _mockCompanyService.CreateCompanyAsync(request, Arg.Any<Guid>())
+            .Returns(companyViewModel);
+
+        // Act
+        var result = await _controller.Create(request);
+
+        // Assert
+        var createdResult = result.Should().BeOfType<CreatedResult>().Subject;
+        createdResult.Location.Should().Be($"/api/company/{companyViewModel.Id}");
+        createdResult.Value.Should().Be(companyViewModel);
+
+        await _mockCreateCompanyvalidator.Received(1).ValidateAsync(request);
+        await _mockCompanyService.Received(1).CreateCompanyAsync(request, Arg.Any<Guid>());
+    }
+}
---
+++ Catering-Platform.Backend/src/Catering.Platform.API/Controllers/CompaniesController.cs
@@ -0,0 +1,45 @@
+﻿using Catering.Platform.Applications.Abstractions;
+using Catering.Platform.Domain.Requests.Company;
+using FluentValidation;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catering.Platform.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CompaniesController : ControllerBase
+    {
+        private readonly IValidator<CreateCompanyRequest> _createCompanyvalidator;
+        private readonly ICompanyService _companyService;
+        private readonly ILogger<CompaniesController> _logger;
+
+        public CompaniesController(
+            IValidator<CreateCompanyRequest> createCompanyvalidator,
+            ICompanyService companyService,
+            ILogger<CompaniesController> logger)
+        {
+            _createCompanyvalidator = createCompanyvalidator;
+            _companyService = companyService;
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] CreateCompanyRequest request)
+        {
+            var tenantId = Guid.NewGuid();
+
+            var validationResult = await _createCompanyvalidator.ValidateAsync(request);
+            if (validationResult.IsValid == false)
+            {
+                _logger.LogInformation(
+                "Validation failed for CreateCompany. Errors: {@ValidationErrors}",
+                validationResult.Errors);
+                return BadRequest(validationResult.Errors);
+            }
+            var companyViewModel = await _companyService.CreateCompanyAsync(request, tenantId);
+            return Created(
+                new Uri($"/api/company/{companyViewModel.Id}", UriKind.Relative),
+                companyViewModel);
+        }
+    }
+}
