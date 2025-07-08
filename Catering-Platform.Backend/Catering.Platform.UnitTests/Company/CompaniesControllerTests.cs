5bacdd71407718b8044bd8f65cdabf172b4f7a89
zola191 <zola19@yandex.ru>
2025-07-08 16:38:18 +0300

    - добавлен endpoint update для контроллера Company - добавлена валидация для request endpoint update - добавлены тесты для endpoint update контроллера Company

--- Catering-Platform.Backend/Catering.Platform.UnitTests/Company/CompaniesControllerTests.cs
+++ Catering-Platform.Backend/Catering.Platform.UnitTests/Company/CompaniesControllerTests.cs
@@ -16,6 +16,7 @@ namespace Catering.Platform.UnitTests.Company;
 public class CompaniesControllerTests
 {
     private readonly IValidator<CreateCompanyRequest> _mockCreateCompanyvalidator;
+    private readonly IValidator<UpdateCompanyRequest> _mockUpdateCompanyvalidator;
     private readonly ICompanyService _mockCompanyService;
     private readonly ILogger<CompaniesController> _mockLogger;
     private readonly CompaniesController _controller;
@@ -24,11 +25,13 @@ public class CompaniesControllerTests
     public CompaniesControllerTests()
     {
         _mockCreateCompanyvalidator = Substitute.For<IValidator<CreateCompanyRequest>>();
+        _mockUpdateCompanyvalidator = Substitute.For<IValidator<UpdateCompanyRequest>>();
         _mockCompanyService = Substitute.For<ICompanyService>();
         _mockLogger = NullLogger<CompaniesController>.Instance;
 
         _controller = new CompaniesController(
             _mockCreateCompanyvalidator,
+            _mockUpdateCompanyvalidator,
             _mockCompanyService,
             _mockLogger);
 
@@ -59,5 +62,58 @@ public class CompaniesControllerTests
 
         await _mockCreateCompanyvalidator.Received(1).ValidateAsync(request);
         await _mockCompanyService.Received(1).CreateCompanyAsync(request, Arg.Any<Guid>());
+    }
+
+    [Fact]
+    public async Task Update_ReturnsOk_WhenUpdateIsSuccessful()
+    {
+        // Arrange
+        var companyId = Guid.NewGuid();
+        var request = _fixture.Build<UpdateCompanyRequest>()
+                            .With(x => x.CompanyId, companyId)
+                            .Create();
+
+        var companyViewModel = _fixture.Create<CompanyViewModel>();
+        var validationResult = new ValidationResult();
+
+        _mockUpdateCompanyvalidator.ValidateAsync(request)
+            .Returns(Task.FromResult(validationResult));
+
+        _mockCompanyService.UpdateCompanyAsync(request, Arg.Any<Guid>())
+            .Returns(companyViewModel);
+
+        // Act
+        var result = await _controller.Update(request);
+
+        // Assert
+        var okResult = result.Should().BeOfType<OkObjectResult>().Subject;
+        okResult.Value.Should().Be(companyViewModel);
+
+        await _mockUpdateCompanyvalidator.Received(1).ValidateAsync(request);
+        await _mockCompanyService.Received(1).UpdateCompanyAsync(request, Arg.Any<Guid>());
+    }
+
+    [Fact]
+    public async Task Update_ReturnsBadRequest_WhenValidationFails()
+    {
+        // Arrange
+        var request = _fixture.Create<UpdateCompanyRequest>();
+        var validationErrors = new List<ValidationFailure>
+        {
+        new ValidationFailure("Name", "Name is required")
+        };
+        var validationResult = new ValidationResult(validationErrors);
+
+        _mockUpdateCompanyvalidator.ValidateAsync(request)
+            .Returns(Task.FromResult(validationResult));
+
+        // Act
+        var result = await _controller.Update(request);
+
+        // Assert
+        var badRequestResult = result.Should().BeOfType<BadRequestObjectResult>().Subject;
+        badRequestResult.Value.Should().BeEquivalentTo(validationResult.Errors);
+
+        await _mockCompanyService.DidNotReceive().UpdateCompanyAsync(Arg.Any<UpdateCompanyRequest>(), Arg.Any<Guid>());
     }
 }
