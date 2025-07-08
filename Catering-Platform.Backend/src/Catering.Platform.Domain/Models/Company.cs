dfe66ddbc677439cb05f4c5ab93719427fc1a94a
zola191 <zola19@yandex.ru>
2025-07-08 08:37:07 +0300

    - Добавлена сущность Company - Добавлена конфигурация через Fluent API в отдельном файле

---
+++ Catering-Platform.Backend/src/Catering.Platform.Domain/Models/Company.cs
@@ -0,0 +1,17 @@
+﻿namespace Catering.Platform.Domain.Models;
+
+public class Company
+{
+    public Guid Id { get; set; }
+    public Guid TenantId { get; set; }
+    public Tenant Tenant { get; set; }
+    public string Name { get; set; }
+    public string TaxNumber { get; set; }
+    public Guid AddressId { get; set; }
+    public Address Address { get; set; }
+    public string? Phone { get; set; }
+    public string? Email { get; set; }
+    public bool IsBlocked { get; set; } = false;
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime? UpdatedAt { get; set; }
+}
---
+++ Catering-Platform.Backend/src/Catering.Platform.Persistence/Configurations/CompanyConfiguration.cs
@@ -0,0 +1,48 @@
+﻿using Catering.Platform.Domain.Models;
+using Catering.Platform.Domain.Shared;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Catering.Platform.Persistence.Configurations;
+
+public class CompanyConfiguration : IEntityTypeConfiguration<Company>
+{
+    public void Configure(EntityTypeBuilder<Company> builder)
+    {
+        builder.ToTable("Company");
+
+        builder.HasKey(f => f.Id);
+
+        builder.Property(f => f.Name)
+            .IsRequired()
+            .HasMaxLength(Constants.MAX_TEXT_LENGTH_200);
+
+        builder.Property(f => f.TaxNumber)
+            .IsRequired()
+            .HasMaxLength(Constants.MAX_TEXT_LENGTH_20);
+
+        builder.Property(f => f.Phone)
+            .IsRequired(false)
+            .HasMaxLength(Constants.MAX_TEXT_LENGTH_20);
+
+        builder.Property(f => f.Email)
+            .IsRequired(false)
+            .HasMaxLength(Constants.MAX_TEXT_LENGTH_100);
+
+        builder.Property(f=>f.IsBlocked)
+            .HasDefaultValue(false);
+
+        builder.HasIndex(f => f.TaxNumber).IsUnique();
+        builder.HasIndex(f => f.TenantId);
+
+        builder.HasOne(e => e.Tenant)
+              .WithMany(t => t.Companies)
+              .HasForeignKey(e => e.TenantId)
+              .OnDelete(DeleteBehavior.Restrict);
+
+        builder.HasOne(e => e.Address)
+              .WithMany(a => a.Companies)
+              .HasForeignKey(e => e.AddressId)
+              .OnDelete(DeleteBehavior.Restrict);
+    }
+}
